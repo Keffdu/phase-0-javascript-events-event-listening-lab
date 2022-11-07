3e733ddd62b499395e2eab1c629775e62e8eb13c
Kevin Dutzy <kddutz15@gmail.com>
2022-11-06 20:32:12 -0500

    commit tree f7c90a24cab18101682fce3d92c1082d59a4ac33

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const input = document.getElementById('button');
+
+
 function addingEventListener() {
+    input.addEventListener('click', function() {
+        console.log("cliqqqued")
+        return "clicked"
+    });
 }
+
+addingEventListener()
